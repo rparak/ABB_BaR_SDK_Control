a34d3c9d76dc389d1feeff502cd58544be602d96
rparak <59449320+rparak@users.noreply.github.com>
2021-05-23 13:51:50 +0100

    upload AS

--- Automation_Studio_BaR/OPCUa_Client_ABB_SDK/Logical/Server_t/Main.c
+++ Automation_Studio_BaR/OPCUa_Client_ABB_SDK/Logical/Server_t/Main.c
@@ -1,61 +1,43 @@
-/* ********************************************************************************************************************** //
-// ---------------------------------------------------------------------------------------------------------------------- //
-// ********************************************************************************************************************** //
+/**
+ * MIT License
+ * Copyright(c) 2020 Roman Parak
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-MIT License
+/**
+ * Author   : Roman Parak
+ * Email    : Roman.Parak@outlook.com
+ * File Name: Main.c
+ * Github   : https://github.com/rparak
+ */
 
-Copyright(c) 2020 Roman Parak
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files(the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions :
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-// ********************************************************************************************************************** //
-// ---------------------------------------------------------------------------------------------------------------------- //
-// ********************************************************************************************************************** //
-
-Author    : Roman Parak
-Email     : Roman.Parak @outlook.com
-Github    : https://github.com/rparak
-File Name : Main.c
-
-// ********************************************************************************************************************** //
-// ---------------------------------------------------------------------------------------------------------------------- //
-// ********************************************************************************************************************** */
-
-
-// ************************************************************************************************************************************ //
-// -------------------------------------------------------------  LIBRARIES ----------------------------------------------------------- //
-// ************************************************************************************************************************************ //
+/** < Include B&R Automation libraries (declarations for B&R ANSI C extensions) */
 #include <bur/plctypes.h>
 
 #ifdef _DEFAULT_INCLUDES
-	#include <AsDefault.h>
+#include <AsDefault.h>
 #endif
 
-// ************************************************************************************************************************************ //
-// -------------------------------------------------------------  DECLARATIONS -------------------------------------------------------- //
-// ************************************************************************************************************************************ //
 // ABB Control // STRUCT // 
 _LOCAL struct abb_m abb_control;
 
-// ************************************************************************************************************************************ //
-// -------------------------------------------------------------  INITIALIZATION ------------------------------------------------------ //
-// ************************************************************************************************************************************ //
+/**
+ * Program Intitialization
+ */
 void _INIT ProgramInit(void)
 {
 	// -------------------- ABB CONTROL {Main} -------------------- //
@@ -114,63 +96,70 @@ void _INIT ProgramInit(void)
 	abb_control.Cartesian.SDK.Trajectory_type = 0;
 }
 
-// ************************************************************************************************************************************ //
-// ----------------------------------------------------------------  MAIN ------------------------------------------------------------- //
-// ************************************************************************************************************************************ //
+/**
+ * Program Cyclic 
+ * 
+ * Duration (Cycle Time): 10000 [µs] 
+ * Tolerance            : 10000 [µs]
+ */
 void _CYCLIC ProgramCyclic(void)
 {
 	
-	// *********************************************************//
-	// ---------------- ABB MAIN STATE  ----------------------- //
-	// *********************************************************//
-	
 	switch(abb_control.state){
-		case 0:
+		case 0: ///<  INITIALIZATION STATE
 			{
-				/*************************************** INITIALIZATION STATE ******************************************/
 				abb_control.state = 0;
 			
-				// Check communication
+				/// Check communication
 				if(abb_control.is_connected == 1){
 					abb_control.state = 10;
 				}
 			}
 			break;
-		case 10:
+		
+		case 10: ///< WAIT STATE
 			{
-				/*************************************** WAIT STATE ******************************************/
 				abb_control.state = 10;
-				// -------------------- START CONTROL {JOINT} -------------------- //
+			
+				/**
+				 * START CONTROL {JOINT} 
+				 */
 				if(abb_control.Command.joint.start == 1){
 					abb_control.state = 31;
 				}
-				// -------------------- START CONTROL {CARTESIAN} -------------------- //
+
+				/**
+				 * START CONTROL {CARTESIAN} 
+				 */
 				if(abb_control.Command.cartesian.start == 1){
 					abb_control.state = 41;
 				}
-				// -------------------- HOME POSITION -------------------- //
+
+				/**
+				 * HOME POSITION
+				 */
 				if(abb_control.Command.cartesian.home == 1 || abb_control.Command.joint.home == 1){
 					abb_control.state = 20;
 				}
 			}
 			break;
-		case 20:
+		
+		case 20: ///< HOME STATE
 			{
-				/*************************************** HOME STATE ******************************************/
 				abb_control.state = 20;
 			
-				// Check in position state
+				/// Check in position state
 				if(abb_control.in_position == 1){
 					abb_control.state = 10;
 				}
 			}
 			break;
-		case 30:
+		
+		case 30: ///< MOVE JOINT STATE (1)
 			{
-				/*************************************** MOVE JOINT STATE (1) ******************************************/
 				abb_control.state = 30;
 			
-				// Start Move {Joint}
+				/// Start Move {Joint}
 				if(abb_control.Command.joint.start == 1){
 					abb_control.state = 31;
 				}else if(abb_control.Command.joint.stop == 1){
@@ -178,12 +167,12 @@ void _CYCLIC ProgramCyclic(void)
 				}
 			}
 			break;
-		case 31:
+		
+		case 31: ///< MOVE JOINT STATE (2)
 			{
-				/*************************************** MOVE JOINT STATE (2) ******************************************/
 				abb_control.state = 31;
 			
-				// Send data {MappView -> SDK}
+				/// Send data {MappView -> SDK}
 				abb_control.Joint.SDK.J1 = abb_control.Joint.Write.J1;
 				abb_control.Joint.SDK.J2 = abb_control.Joint.Write.J2;
 				abb_control.Joint.SDK.J3 = abb_control.Joint.Write.J3;
@@ -193,18 +182,18 @@ void _CYCLIC ProgramCyclic(void)
 				abb_control.Joint.SDK.Velocity = abb_control.Joint.Write.Velocity;
 				abb_control.Joint.SDK.Zone     = abb_control.Joint.Write.Zone;
 			
-				// Check in position state
+				/// Check in position state
 				if(abb_control.in_position == 1){
 					abb_control.state = 30;
 				}
 			}
 			break;
-		case 40:
+		
+		case 40: ///< MOVE CARTESIAN STATE (1)
 			{
-				/*************************************** MOVE CARTESIAN STATE (1) ******************************************/
 				abb_control.state = 40;
 			
-				// Start Move {Cartesian}
+				/// Start Move {Cartesian}
 				if(abb_control.Command.cartesian.start == 1){
 					abb_control.state = 41;
 				}else if(abb_control.Command.cartesian.stop == 1){
@@ -212,12 +201,12 @@ void _CYCLIC ProgramCyclic(void)
 				}
 			}
 			break;
-		case 41:
+		
+		case 41: ///< MOVE CARTESIAN STATE (2)
 			{
-				/*************************************** MOVE CARTESIAN STATE (2) ******************************************/
 				abb_control.state = 41;
 			
-				// Send data {MappView -> SDK}
+				/// Send data {MappView -> SDK}
 				abb_control.Cartesian.SDK.X = abb_control.Cartesian.Write.X;
 				abb_control.Cartesian.SDK.Y = abb_control.Cartesian.Write.Y;
 				abb_control.Cartesian.SDK.Z = abb_control.Cartesian.Write.Z;
@@ -228,7 +217,7 @@ void _CYCLIC ProgramCyclic(void)
 				abb_control.Cartesian.SDK.Zone = abb_control.Cartesian.Write.Zone;
 				abb_control.Cartesian.SDK.Trajectory_type = abb_control.Cartesian.Write.Trajectory_type;
 			
-				// Check in position state
+				/// Check in position state
 				if(abb_control.in_position == 1){
 					abb_control.state = 40;
 				}
@@ -236,7 +225,9 @@ void _CYCLIC ProgramCyclic(void)
 			break;
 	}
 	
-	// -------------------- DEFAULT PARAM. {JOINT} -------------------- //
+	/**
+	 * DEFAULT PARAM. {JOINT}
+	 */
 	if(abb_control.Command.joint.default_parameters == 1){
 		abb_control.Joint.Write.J1 = 0;
 		abb_control.Joint.Write.J2 = 0;
@@ -245,7 +236,10 @@ void _CYCLIC ProgramCyclic(void)
 		abb_control.Joint.Write.J5 = 90;
 		abb_control.Joint.Write.J6 = 0;
 	}
-	// -------------------- DEFAULT PARAM. {CARTESIAN	} -------------------- //
+
+	/**
+	 * DEFAULT PARAM. {CARTESIAN}
+	 */
 	if(abb_control.Command.cartesian.default_parameters == 1){
 		abb_control.Cartesian.Write.X = 302;
 		abb_control.Cartesian.Write.Y = 0;
